b8a8d74fc59fcf9d4cf1c6e25dfcdc04744ad4e4
turpp <white.tripp@icloud.com>
2020-12-15 23:25:48 -0600

    added a show page for user and added links to the show page for tournaments

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,2 +1,21 @@
 class TournamentsController < ApplicationController
+    def index
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def show
+        @tournament=Tournament.find_by(id: params[:id])
+        @players=@tournament.players
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+    
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def show
+        @user=User.find_by(id: params[:id])
+        @tournaments=@user.tournaments
+        @players=@user.players
+    end
+
+
 end
