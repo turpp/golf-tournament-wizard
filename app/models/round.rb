db7467b76e8a273d6c9eaa94b7d8a967c518a3de
turpp <white.tripp@icloud.com>
2020-12-20 06:10:52 -0600

    the posting page now sums up rounds and also total score for the tournament

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,4 +1,14 @@
 class Round < ApplicationRecord
   belongs_to :team
   has_many :holes
+
+
+  def round_total
+  hole_scores=[]
+    self.holes.each do |hole|
+      hole_scores << hole.score
+    end
+    self.score=hole_scores.inject(0){|sum,x| sum + x }
+  end
+
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -8,4 +8,13 @@ class Team < ApplicationRecord
   
   accepts_nested_attributes_for :players
   accepts_nested_attributes_for :players_teams
+
+
+  def total_score
+    total=[]
+    self.rounds.each do |round|
+      total << round.score
+    end
+    self.final_score=total.inject(0){|sum,x| sum + x }
+  end
 end
