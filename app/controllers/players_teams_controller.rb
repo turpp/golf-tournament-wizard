01b4b822e757386feb406f11e8edcf2522654c7d
turpp <white.tripp@icloud.com>
2020-12-21 00:26:28 -0600

    added session[:user_id] checks for PlayersTeam

--- app/controllers/players_teams_controller.rb
+++ app/controllers/players_teams_controller.rb
@@ -1,10 +1,18 @@
 class PlayersTeamsController < ApplicationController
+    before_action :require_login
+
+    
     def checkin
+        if helpers.current_user.tournament_ids.include?(params[:tournament_id].to_i)
         @tournament=Tournament.find_by(id: params[:tournament_id])
         @teams=@tournament.teams
         @n=0
         @team=Team.new
         @all=[]
+        else
+            redirect_to root_path, alert: "You can't do that!"
+        end
+
     end
 
     def update
@@ -20,4 +28,9 @@ class PlayersTeamsController < ApplicationController
     def players_team_params
         params.require(:players_team).permit(:paid, :mulligan)
     end
+
+    def require_login
+        return head(:forbidden) unless session.include? :user_id
+      end
+
 end
