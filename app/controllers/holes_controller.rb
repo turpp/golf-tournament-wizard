e50aff7eee56a00373637d943eca5c61e6f5f761
turpp <white.tripp@icloud.com>
2020-12-22 00:08:14 -0600

    added validations for holes

--- app/controllers/holes_controller.rb
+++ app/controllers/holes_controller.rb
@@ -8,16 +8,20 @@ class HolesController < ApplicationController
     end
 
     def create
-        byebug
         team=Team.find_by(id: params[:round][:team_id].to_i)
-        if helpers.current_user.tournament_ids.include?(team.tournament_id)
-        round=Round.create(round_params)
-            if !round.blank?
-                params[:hole].each do |hole|
-                @hole=Hole.create(hole_params(hole))
-                round.holes << @hole
-                end
+        params[:hole].each do |hole|
+            if hole[:score].blank?
+                return redirect_to "/holes/round_entry/teams/#{team.id}", alert: "Please enter a score for each hole before submiting."
             end
+        end
+        if helpers.current_user.tournament_ids.include?(team.tournament_id)
+            round=Round.create(round_params)
+                if !round.blank?
+                    params[:hole].each do |hole|
+                    @hole=Hole.create(hole_params(hole))
+                    round.holes << @hole
+                    end
+                end
         end
         redirect_to "/tournaments/#{team.tournament.id}/posting"
     end
