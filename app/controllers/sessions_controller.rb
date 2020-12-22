f994b73e6888af1cc82d1073a197250fcded17cb
turpp <white.tripp@icloud.com>
2020-12-21 20:30:24 -0600

    added validations to the login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,29 +5,31 @@ class SessionsController < ApplicationController
     def new
     end
 
-    # def create
-    #     @user = User.find_by(username: params[:username])
-    #     return head(:forbidden) unless @user.authenticate(params[:password])
-    #     session[:user_id] = @user.id
-    #     redirect_to user_path(@user)
-    # end
 
     def create
         if params[:username]
-        @user = User.find_by(username: params[:username])
-        return head(:forbidden) unless @user.authenticate(params[:password])
-        session[:user_id] = @user.id
-        redirect_to user_path(@user)
+            if params[:username].blank? || params[:password].blank?    
+                redirect_to '/login', alert: "Must fill out Username and Password Fields"
+
+            else
+                if @user = User.find_by(username: params[:username])
+                return redirect_to '/login', alert: "Wrong Username and Password Combination" unless @user.authenticate(params[:password])
+                session[:user_id] = @user.id
+                redirect_to user_path(@user)
+                else
+                    redirect_to '/login', alert: "Inncorrect Username"
+                end
+
+            end
         else
         @user = User.find_or_create_by(uid: auth['uid']) do |u|
             u.name = auth['info']['name']
             u.email = auth['info']['email']
             u.image = auth['info']['image']
             u.password = auth['uid']
-            
-        end
+            end
         session[:user_id] = @user.id
-            redirect_to user_path(@user)
+         redirect_to user_path(@user)
         end
 
     end
