a27f14169934c68ef35730de9376d4cd5ce78414
turpp <white.tripp@icloud.com>
2020-12-30 14:43:29 -0600

    removed unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,45 @@
 Rails.application.routes.draw do
-  resources :rounds
+  # resources :rounds
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  #nested= /tournaments/:tournament_id/teams/times
   post '/teams/times/:tournament_id', to: 'teams#times'
-  get '/teams/signup/:tournament_id', to: "teams#signup"
+  #-------------------------
+
+  #nested /tournament/:tournament_id/players_teams/checkin
   get '/players_teams/checkin/:tournament_id', to: "players_teams#checkin"
+  #-------------------------------------------------
   get "/players/number", to: 'players#number'
+
+  #nested /tournament/:tournament_id/teams/number
   get '/teams/number/:tournament_id', to: 'teams#number'
   post '/teams/number', to: 'teams#times' 
+  #-----------------
+  #nested= /teams/:team_id/holes/round_entry
   get '/holes/round_entry/teams/:team_id', to: 'holes#round_entry'
-  get '/teams/round/:team_id', to: "teams#round"
+  #-------------------
+
   get '/tournaments/:touranment_id/posting', to: "tournaments#posting"
   get '/tournaments/:tournament_id/results', to: "tournaments#results"
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   get '/auth/facebook/callback' => 'sessions#create' 
-  resources :tournaments
+  resources :tournaments #need nested team items
   resources :users
   resources :players
-  resources :teams
-  resources :players_teams
-  resources :holes
+  resources :teams, except: :index
+  resources :players_teams, only: :update
+  resources :holes, only: :create
   root "sessions#home"
+
+
   get "/players/new/:number", to: "players#new"
   post "/players/number", to: 'players#times'
-  get '/teams/tournament/:id', to: 'teams#tournament'
+
+#nested= /tournament/:touranment_id/teams/new/:number
   get '/teams/new/:number/:tournament_id', to: 'teams#new'
 end
