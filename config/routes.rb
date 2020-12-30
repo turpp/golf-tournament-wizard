00e2231cc0f250a2eb513d78c2987acafa390212
turpp <white.tripp@icloud.com>
2020-12-30 17:21:08 -0600

    finished up cleaning up routes with correct nested routes:

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   #nested= /tournaments/:tournament_id/teams/times
-  post '/teams/times/:tournament_id', to: 'teams#times' 
+  # post '/teams/times/:tournament_id', to: 'teams#times' 
   #-------------------------
 
   #nested /tournament/:tournament_id/players_teams/checkin
@@ -13,7 +13,11 @@ Rails.application.routes.draw do
 
   #nested /tournament/:tournament_id/teams/number
   # get '/teams/number/:tournament_id', to: 'teams#number'
-  post '/teams/number', to: 'teams#times' 
+
+
+  # post '/teams/number', to: 'teams#times' 
+
+
   #-----------------
   #nested= /teams/:team_id/holes/round_entry
   # get '/holes/round_entry/teams/:team_id', to: 'holes#round_entry'
@@ -28,18 +32,17 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   get '/auth/facebook/callback' => 'sessions#create' 
-  resources :tournaments do#need nested team,player_team checkin, 
-    # post '/teams/times/:tournament_id', to: 'teams#times'
-    # post '/teams/number', to: 'teams#times' 
+
+  resources :tournaments do
     get '/teams/new/:number', to: 'teams#new'
     get '/teams/number', to: 'teams#number'
     get '/players_teams/checkin', to: "players_teams#checkin"
+    post '/teams/number', to: 'teams#times'
   end
   
   resources :users
   resources :players
   resources :teams, except: :index  do #hole round entry
-    # get '/holes/round_entry/teams/:team_id', to: 'holes#round_entry'
     get '/holes/round_entry', to: 'holes#round_entry'
     resources :holes, only: :create
   end
