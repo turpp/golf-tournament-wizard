c4bee6f613525a22deb53ed7c54aa40c17b52066
turpp <white.tripp@icloud.com>
2020-12-29 23:24:02 -0600

    refactored players controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -9,7 +9,6 @@ class PlayersController < ApplicationController
     end
 
     def times
-
         @n=params[:number].to_i
         if @n > 0
             redirect_to "/players/new/#{@n}"
@@ -25,26 +24,34 @@ class PlayersController < ApplicationController
 
     def create
         @n=params[:number].to_i
-        params[:player].each do |player|
-            @player=Player.create(player_params(player))
-            if !@player.valid?
-                return render :new
-            end
-        end
+        if create_players(params[:player])
+        # params[:player].each do |player|
+        #     @player=Player.create(player_params(player))
+        #     if !@player.valid?
+        #         return render :new
+        #     end
+        # end
+        else
         redirect_to players_path
+        end
     end
     
     def show
-        if helpers.current_user.player_ids.include?(params[:id].to_i)
-            @player=Player.find_by(id: params[:id])
+        # if helpers.current_user.player_ids.include?(params[:id].to_i)
+        if authorized(params[:id])
+
+            # @player=Player.find_by(id: params[:id])
+            @player=find_player
         else
             redirect_to root_path, alert: "You can't do that!"
         end
     end
 
     def edit
-        if helpers.current_user.player_ids.include?(params[:id].to_i)
-            @player=Player.find_by(id: params[:id])
+        # if helpers.current_user.player_ids.include?(params[:id].to_i)
+        if authorized(params[:id])
+            # @player=Player.find_by(id: params[:id])
+            @player=find_player
             @player_edit=Player.new
         else
             redirect_to root_path, alert: "You can't do that!"
@@ -52,7 +59,8 @@ class PlayersController < ApplicationController
     end
 
     def update
-        @player=Player.find_by(id: params[:id])
+        # @player=Player.find_by(id: params[:id])
+        @player=find_player
         if @player.update(player_params(params[:player]))
             redirect_to player_path(@player)
         else
@@ -61,7 +69,8 @@ class PlayersController < ApplicationController
     end
 
     def destroy
-        Player.find(params[:id]).destroy
+        # Player.find(params[:id]).destroy
+        find_player.destroy
         redirect_to players_path
     end
 
@@ -73,6 +82,24 @@ class PlayersController < ApplicationController
 
     def require_login
         return head(:forbidden) unless session.include? :user_id
+    end
+
+    def create_players(params)
+        params.each do |player|
+            @player=Player.create(player_params(player))
+            if !@player.valid?
+                return render :new
+            end
+        end
+        redirect_to players_path
+    end
+
+    def authorized(params)
+        helpers.current_user.player_ids.include?(params.to_i)
+    end
+
+    def find_player
+        Player.find_by(id: params[:id])
     end
 
 end
